655b7adaf68c8d5e017622a47ac3b541eae462c7
sdoyle88 <r3tr0pi3@gmail.com>
2021-05-03 11:30:09 -0700

    updating name of folder

---
+++ code/ch7-databases-yourturn/data/user.py
@@ -0,0 +1,13 @@
+import datetime
+import sqlalchemy as sa
+from data.modelbase import SqlAlchemyBase
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String)
+    email = sa.Column(sa.String, index=True, unique=True)
+    hash_password = sa.Column(sa.String)
+    created_date = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    last_login = sa.Column(sa.DateTime, default=datetime.datetime.now, index=True)
+    profile_image_url = sa.Column(sa.String)
